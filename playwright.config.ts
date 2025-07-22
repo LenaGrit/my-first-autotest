f619737a8f22fecadf7409d0013c42e86b0c72dd
LenaGrit <9617656341@mail.ru>
2025-07-22 11:58:41 +0500

    С визуалом headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  timeout: 30000,
-  retries: 0,
+  testDir: './tests', //папка, из которой запускается тест
+  timeout: 30000, //сколько тест ищет в милисек
+  workers: 1, //количество параллельных запусков автотестов (если они не зависят друг от друга, 
+  // но и комп должен смочь одновременно обрабатывать эти запросы, то есть не всегда это сокращает время тестов)
+  retries: 0, //количество перезапусков теста
   reporter: [['list'], ['html']],
   use: {
     baseURL: 'https://www.saucedemo.com',
-    headless: true,
+    headless: false, //режим, в котором браузер запускается с видимым окном, а не "в фоне".
+    //true - невидимо, false - открытие "вживую")
   },
   projects: [
     {
